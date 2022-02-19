10d1ba1c5b01627793f294f89fe7e0cd433242a7
Javier Urbina <xavyurbina88@gmail.com>
2022-02-19 16:37:08 -0600

    Implementing Edit Category functionality

--- BookAppWeb/Controllers/CategoryController.cs
+++ BookAppWeb/Controllers/CategoryController.cs
@@ -41,5 +41,39 @@ namespace BookAppWeb.Controllers
             }
             return View(obj);
         }
+
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder Cannot exactly match the Name.");
+
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
--- BookAppWeb/Models/Category.cs
+++ BookAppWeb/Models/Category.cs
@@ -8,6 +8,8 @@ namespace BookAppWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [Display(Name ="Display Order")]
+        [Range(1,100,ErrorMessage ="Display order must be between 1 and 100 only!")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
