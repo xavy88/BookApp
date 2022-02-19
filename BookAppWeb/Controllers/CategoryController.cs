bdd47fa284309f651b663e9088e1a112ba9c5050
Javier Urbina <xavyurbina88@gmail.com>
2022-02-19 16:01:06 -0600

    Adding Creating Category Functionality

--- BookAppWeb/Controllers/CategoryController.cs
+++ BookAppWeb/Controllers/CategoryController.cs
@@ -17,5 +17,29 @@ namespace BookAppWeb.Controllers
             IEnumerable<Category> objCategoryList=_db.Categories;
             return View(objCategoryList);
         }
+
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder Cannot exactly match the Name.");
+                
+            }
+            if (ModelState.IsValid) { 
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
