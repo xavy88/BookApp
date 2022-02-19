378bcf4057768403a3e2ef071f929d13aa596115
Javier Urbina <xavyurbina88@gmail.com>
2022-02-18 18:15:16 -0600

    Creating CategoryContoller and list of categories view

---
+++ BookAppWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BookAppWeb.Data;
+using BookAppWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookAppWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList=_db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
