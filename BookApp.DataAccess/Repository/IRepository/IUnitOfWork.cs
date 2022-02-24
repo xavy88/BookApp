c3c4b42dac3f2faa9727db190f167ca5f0f06134
Javier Urbina <xavyurbina88@gmail.com>
2022-02-23 20:36:05 -0600

    Implementing IUnitOfWork and UnitOfWork

--- BookApp.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BookApp.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -1,5 +1,4 @@
-﻿using BookApp.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace BookApp.DataAccess.Repository.IRepository
 {
-    public interface ICategoryRepository:IRepository<Category>
+    public interface IUnitOfWork
     {
-        void Update(Category obj);
-       
+        ICategoryRepository Category { get; }
+        void Save();
     }
 }
--- BookApp.DataAccess/Repository/UnitOfWork.cs
+++ BookApp.DataAccess/Repository/UnitOfWork.cs
@@ -1,5 +1,4 @@
 ﻿using BookApp.DataAccess.Repository.IRepository;
-using BookApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,18 +7,20 @@ using System.Threading.Tasks;
 
 namespace BookApp.DataAccess.Repository
 {
-    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    public class UnitOfWork : IUnitOfWork
     {
         private ApplicationDbContext _db;
 
-        public CategoryRepository(ApplicationDbContext db):base(db)
+        public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
+            Category = new CategoryRepository(_db);
         }
-     
-        public void Update(Category obj)
+        public ICategoryRepository Category { get; private set; }
+
+        public void Save()
         {
-            _db.Categories.Update(obj);
+            _db.SaveChanges();  
         }
     }
 }
