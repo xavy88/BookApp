e5c7a3dc613b456031dcdcc12ebce72c57ce2688
Javier Urbina <xavyurbina88@gmail.com>
2022-02-25 17:48:15 -0600

    Adding Product table to the DB

--- BookApp.DataAccess/Data/ApplicationDbContext.cs
+++ BookApp.DataAccess/Data/ApplicationDbContext.cs
@@ -12,5 +12,6 @@ namespace BookApp.DataAccess
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
--- BookApp.Model/Product.cs
+++ BookApp.Model/Product.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BookApp.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,40 @@ namespace BookApp.Model
 {
     public class Product
     {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Range(1,1000)]
+        public double ListPrice { get; set; }
+
+        [Required]
+        [Range(1, 1000)]
+        public double Price { get; set; }
+
+
+        [Required]
+        [Range(1, 1000)]
+        public double Price50 { get; set; }
+
+
+        [Required]
+        [Range(1, 1000)]
+        public double Price100 { get; set; }
+
+        public string ImageURL { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        [Required]
+        public int CoverTypeId { get; set; }
+        [ForeignKey("CoverTypeId")]
+        public Category CoverType { get; set; }
     }
 }
