aa553cf5b16ddfd8551c9487a12b1b6501a00777
Javier Urbina <xavyurbina88@gmail.com>
2022-03-03 17:54:10 -0600

    Implementing Details functionality

--- BookAppWeb/Areas/Customer/Controllers/HomeController.cs
+++ BookAppWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using BookApp.Models;
+﻿using BookApp.DataAccess.Repository.IRepository;
+using BookApp.Model;
+using BookApp.Model.ViewModels;
+using BookApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +11,28 @@ namespace BookAppWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
+            return View(productList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            ShoppingCart cartObj = new()
+            {
+                Count = 1,
+                Product = _unitOfWork.Product.GetFirstOrdefault(u => u.Id == id, includeProperties: "Category,CoverType")
+            };
+            return View(cartObj);
         }
 
         public IActionResult Privacy()
